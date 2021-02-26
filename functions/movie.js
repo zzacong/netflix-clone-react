648370e3613098b27ed66fe7336dcab00917e124
zzacong <ongzz100@gmail.com>
2021-02-27 02:38:35 +1100

    fix: redirects pass 4

--- functions/movie.js
+++ functions/movie.js
@@ -4,17 +4,17 @@ if (process.env.NODE_ENV !== 'production')
   require('dotenv').config({ path: path.resolve(process.cwd(), '.env.local') })
 
 exports.handler = async function (event, _context) {
-  const { path, queryStringParameters } = event
-
-  const url = new URL(`https://api.themoviedb.org/3/${path.slice(11)}`)
-
-  url.searchParams.append('api_key', process.env.TMDB_API_KEY)
-
-  if (Object.keys(queryStringParameters).length) {
-    const [q] = Object.entries(queryStringParameters)
-    url.searchParams.append(q?.[0], q?.[1])
-  }
   try {
+    const { path, queryStringParameters } = event
+
+    const url = new URL(`https://api.themoviedb.org/3/${path.slice(11)}`)
+
+    url.searchParams.append('api_key', process.env.TMDB_API_KEY)
+
+    if (Object.keys(queryStringParameters).length) {
+      const [q] = Object.entries(queryStringParameters)
+      url.searchParams.append(q?.[0], q?.[1])
+    }
     const { data } = await axios.get(url.href)
     return {
       statusCode: 200,
